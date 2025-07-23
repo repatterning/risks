704e9779b97596b026bf9376cbe489e892a48bc6
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-07-23 21:25:35 +0100

    Update src/algorithms/structures.py

--- src/algorithms/structures.py
+++ src/algorithms/structures.py
@@ -1,10 +1,6 @@
 """Module structures.py"""
-import logging
 
-import dask
 import pandas as pd
-
-import src.algorithms.persist
 
 
 class Structures:
@@ -24,33 +20,9 @@ class Structures:
         self.__fields = ['maximum', 'minimum', 'latest', 'median', 'points', 'catchment_id', 'ts_id',
                          'station_name', 'catchment_name', 'latitude', 'longitude', 'river_name']
 
-    @dask.delayed
-    def __get_section(self, ending: int) -> pd.DataFrame:
-        """
-
-        :param ending: The weighted rates of change are with respect to an ending timestamp
-        :return:
-        """
-
-        return self.__instances.copy().loc[
-            self.__instances['ending'] == ending, self.__fields]
 
     def exc(self):
         """
 
         :return:
         """
-
-        endings = self.__instances['ending'].unique()
-
-        # Delayed Tasks
-        __persist = dask.delayed(src.algorithms.persist.Persist().exc)
-
-        computations = []
-        for ending in endings:
-            section = self.__get_section(ending=int(ending))
-            message = __persist(section=section, ending=int(ending))
-            computations.append(message)
-        messages = dask.compute(computations, scheduler='threads')[0]
-
-        logging.info(messages)
