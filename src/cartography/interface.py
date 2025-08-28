4e2b719847cdfd99d89b9c61f38709bab146b488
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-08-28 22:19:44 +0100

    Update src/cartography/interface.py

--- src/cartography/interface.py
+++ src/cartography/interface.py
@@ -1,4 +1,6 @@
 
+import geopandas
+
 import pandas as pd
 
 class Interface:
@@ -6,3 +8,10 @@ class Interface:
     def __init__(self, reference: pd.DataFrame):
 
         self.__reference = reference
+
+    def exc(self):
+
+        attributes = geopandas.GeoDataFrame(
+            self.__reference,
+            geometry=geopandas.points_from_xy(self.__reference['longitude'], self.__reference['latitude'])
+        )
