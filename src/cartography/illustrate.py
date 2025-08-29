344f94f37eb4c43e311c38ab194ce40c1bfd41e4
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-08-29 14:18:47 +0100

    Update src/cartography/illustrate.py: In Progress

--- src/cartography/illustrate.py
+++ src/cartography/illustrate.py
@@ -1,8 +1,13 @@
 
+import geopandas
+
+
 class Illustrate:
 
-    def __init__(self):
-        pass
+    def __init__(self, data: geopandas.GeoDataFrame, coarse: geopandas.GeoDataFrame):
+
+        self.__data = data
+        self.__coarse = coarse
 
     def exc(self):
         pass
