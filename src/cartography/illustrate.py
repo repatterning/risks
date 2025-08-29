b3a5d3f63511ebe69a010ca6885a90f8f2e8aa8e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-08-29 16:48:08 +0100

    Update src/cartography/interface.py

--- src/cartography/illustrate.py
+++ src/cartography/illustrate.py
@@ -1,5 +1,11 @@
 
+import logging
+
+import branca.colormap
 import geopandas
+
+import src.cartography.parcels
+import src.elements.parcel as pcl
 
 
 class Illustrate:
@@ -10,4 +16,10 @@ class Illustrate:
         self.__coarse = coarse
 
     def exc(self):
-        pass
+
+        parcels: list[pcl.Parcel] = src.cartography.parcels.Parcels(data=self.__data).exc()
+        colours: branca.colormap.StepColormap = branca.colormap.linear.YlOrBr_09.to_step(len(parcels))
+        logging.info(colours)
+
+        for parcel in parcels:
+            logging.info(parcel)
