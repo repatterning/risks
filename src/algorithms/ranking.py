8d4952c4690546163f88c2d1ecb3b50b2dee667a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-12-31 15:55:43 +0000

    Add src/algorithms/ranking.py: In Progress

---
+++ src/algorithms/ranking.py
@@ -0,0 +1,48 @@
+import logging
+
+import pandas as pd
+
+
+class Ranking:
+
+    def __init__(self):
+        pass
+
+    def __calculate(self, data: pd.DataFrame) -> pd.DataFrame:
+        """
+
+        :return:
+        """
+
+        frame = data.copy()[['catchment_id', 'catchment_name', 'latest']].groupby(
+            by=['catchment_id', 'catchment_name']).agg(maximum=('latest', 'max'))
+        logging.info(frame)
+
+        # Convert 'catchment_id' & 'catchment_name' to fields; currently indices.
+        frame.reset_index(drop=False, inplace=True)
+
+        # Hence
+        frame['rank'] = frame['maximum'].rank(method='first', ascending=False).astype(int)
+        frame.drop(columns='maximum', inplace=True)
+        frame.sort_values(by='catchment_name', inplace=True)
+        frame.reset_index(drop=True, inplace=True)
+        logging.info(frame)
+
+        return frame
+
+    def exc(self, instances: pd.DataFrame):
+        """
+
+        :param instances:
+        :return:
+        """
+
+
+        __points = instances['points'].unique()
+        logging.info(__points)
+
+        for points in __points:
+            data = instances.copy().loc[instances['points'] == points, :]
+            logging.info(data)
+
+            self.__calculate(data=data)
