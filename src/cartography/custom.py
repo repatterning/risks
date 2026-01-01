90a794a3c274c6a9b9bea2c7bb36ba14b1cf5086
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-01-01 01:03:38 +0000

    Update src/cartography/custom.py: In Progress

--- src/cartography/custom.py
+++ src/cartography/custom.py
@@ -11,34 +11,48 @@ class Custom:
         pass
 
     @staticmethod
-    def f_radius(value: float) -> float:
+    def f_radius(value: float, lower: float, upper: float) -> float:
         """
 
         arctan: minimum + (maximum - minimum)*2*np.arctan(0.5*value*np.pi)/np.pi
         tanh: minimum + (maximum - minimum)*np.tanh(value)
 
         :param value:
+        :param lower:
+        :param upper:
         :return:
         """
 
-        minimum = 13.5
-        maximum = 22.5
+        minimum = 3.5
+        maximum = 33.5
 
-        est = minimum + (maximum - minimum)*np.abs(value)/(1 + np.abs(value))
+        # factor = value/(1 + np.abs(value))
+        # est = minimum + factor*(maximum - minimum)
+
+        factor = (value - lower)/(1 + upper - lower)
+        est = minimum + factor*(maximum - minimum)
 
         return est
 
     @staticmethod
-    def f_opacity(value: float) -> float:
+    def f_opacity(value: float, lower: float, upper: float) -> float:
         """
+        (value + 1)/np.sqrt(1 + np.power(value, 2))
 
         :param value:
+        :param lower:
+        :param upper:
         :return:
         """
 
         minimum = 0.25
         maximum = 0.95
 
-        est = minimum + (maximum - minimum)*(value + 1)/np.sqrt(1 + np.power(value, 2))
+        est = minimum + (maximum - minimum)*(value - lower)/(1 + upper - lower)
 
         return est
+
+    @staticmethod
+    def f_rule(value: float) -> str:
+
+        return 'odd' if value < 0 else 'even'
