b99f1470c8c5405edc9db819f3cea25c7d127693
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-01-01 01:26:11 +0000

    Update src/cartography/illustrate.py: In Progress

--- src/cartography/illustrate.py
+++ src/cartography/illustrate.py
@@ -75,19 +75,24 @@ class Illustrate:
 
             # The instances of a catchment
             instances = self.__data.copy().loc[self.__data['catchment_id'] == parcel.catchment_id, :]
+            lower = instances['latest'].min()
+            upper = instances['latest'].max()
 
             # Draw
             folium.GeoJson(
                 data = instances.to_crs(epsg=3857),
                 name=f'{parcel.catchment_name}',
                 marker=folium.CircleMarker(
-                    radius=22.5, stroke=False, fill=True, fillColor=colours(parcel.decimal), fill_opacity=0.65),
+                    radius=22.5, weight=2, color=colours(parcel.decimal), fillColor=colours(parcel.decimal), fill_opacity=0.65),
                 tooltip=folium.GeoJsonTooltip(
                     fields=['latest', 'maximum', 'median', 'station_name', 'river_name', 'catchment_name'],
                     aliases=['latest (mm/hr)', 'maximum (mm/hr)', 'median (mm/hr)', 'Station', 'River/Water', 'Catchment']),
                 style_function=lambda feature: {
-                    "fillOpacity": custom.f_opacity(feature['properties']['latest']),
-                    "radius": custom.f_radius(feature['properties']['latest'])
+                    "fillOpacity": custom.f_opacity(feature['properties']['latest'], lower=lower, upper=upper),
+                    "opacity": custom.f_opacity(feature['properties']['latest'], lower=lower, upper=upper),
+                    "radius": custom.f_radius(feature['properties']['latest'], lower=lower, upper=upper),
+                    "stroke": custom.f_stroke(feature['properties']['latest']),
+                    "fill": custom.f_fill(feature['properties']['latest'])
                 },
                 zoom_on_click=True,
                 show=show
